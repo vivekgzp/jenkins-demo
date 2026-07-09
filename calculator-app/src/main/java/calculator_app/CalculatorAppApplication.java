af9145d87c4d7a7f6f2d96db2da774ab461679fb
vivekgzp <sharmavivekgzp@gmail.com>
2026-07-09 17:04:42 +0000

    Adding springboot

---
+++ calculator-app/src/main/java/calculator_app/CalculatorAppApplication.java
@@ -0,0 +1,13 @@
+package calculator_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorAppApplication.class, args);
+	}
+
+}
